93015caf09653648366621808ead3646105e9ffd
fidelram <fidel.ramirez@gmail.com>
2016-10-29 07:28:40 +0200

    adding file

---
+++ bin/assemble.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import argparse
+
+import numpy as np
+
+import parserCommon
+
+import hicexplorer.HiCMatrix as HiCMatrix
+import hicexplorer.HiCAssembler_clean_copy as HiCAssembler
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import logging as log
+
+log.basicConfig(level=log.DEBUG)
+
+debug = 0
+
+TEMP_FOLDER = '/tmp/'
+
+
+def parse_arguments(args=None):
+    parent_parser = parserCommon.getParentArgParse()
+
+    parser = argparse.ArgumentParser(
+        parents=[parent_parser],
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='The idea is to test whether the contigs '
+        'align next to each other. The format of '
+        'the matrix row labels should include '
+        'the position of the contig.')
+
+    parser.add_argument('--outFile', '-o',
+                        help='prefix for output files.',
+                        required=True)
+
+
+    return(parser.parse_args(args))
+
+
+def main(args):
+    # load matrix
+    basename = args.outFile
+    ma = HiCMatrix.hiCMatrix(args.matrix)
+    assembl = HiCAssembler.HiCAssembler(ma)
+
+    assembl.assemble_contigs()
+    data = {'scaffolds':assembl.N50,
+            'id2contig_pos': assembl.hic.cut_intervals,
+            'contig_name': assembl.scaffolds.id2contig_name,
+            'id2pos': position}
+
+    make_plots(data, args.outFile, args.genomeSizes)
+
+#    scaffolds_to_bed(assembl, position)
+    import pickle as pi
+    with open(args.outFile+'_final_assembl.pickle', 'wb') as f:
+        pi.dump(data,f)
+    import ipdb;ipdb.set_trace()
+    exit()
+    print_N50(data['scaffolds'], args.outFile)
+    test_super_contigs(assembl, args.outFile,position)
+    exit()
+    print np.array([(k, v) for k, v in id2pos.iteritems()])
+
+#    mat = ma.matrix[new_order,:][:,new_order]
+#    np.savez("/tmp/ordermat.npz", mat=mat)
+#    ma.setMatrixValues(mat)
+#    ma.save(args.outFile.name)
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
